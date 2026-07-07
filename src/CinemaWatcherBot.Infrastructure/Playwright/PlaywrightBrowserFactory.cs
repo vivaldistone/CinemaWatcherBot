b961263f4f9d5f4e44b36cc68cfffdba457984b3
vivaldistone <pipenabsolute@icloud.com>
2026-07-07 18:51:56 +0300

    refactor : add semaphore slim for sync

--- src/CinemaWatcherBot.Infrastructure/Playwright/PlaywrightBrowserFactory.cs
+++ src/CinemaWatcherBot.Infrastructure/Playwright/PlaywrightBrowserFactory.cs
@@ -5,19 +5,32 @@ namespace CinemaWatcherBot.Infrastructure.Playwright;
 public class PlaywrightBrowserFactory : IPlaywrightBrowserFactory
 {
     private IBrowser? _browser;
+    private readonly SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1,1);
     public async Task<IBrowser> GetAsync(CancellationToken token = default)
     {
         if (_browser is not null)
             return _browser;
 
-        var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+        await semaphoreSlim.WaitAsync();
 
-        _browser =  await playwright.Chromium.LaunchAsync(
-            new BrowserTypeLaunchOptions
-            {
-                Headless = true
-            });
+        try
+        {
+            if (_browser is not null)
+                return _browser;
 
+            var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+
+            _browser =  await playwright.Chromium.LaunchAsync(
+                new BrowserTypeLaunchOptions
+                {
+                    Headless = true
+                });
+        }
+        finally
+        {
+            semaphoreSlim.Release();
+        }
+        
         return _browser;
     }
 }
