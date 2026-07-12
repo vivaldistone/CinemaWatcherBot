340708ca3850ba930a6f9ec4ab2b9ce3b554f1d9
vivaldistone <pipenabsolute@icloud.com>
2026-07-12 23:11:05 +0300

    feat: add movie session entity

---
+++ src/CinemaWatcherBot.Domain/Entities/MovieSession.cs
@@ -0,0 +1,33 @@
+﻿namespace CinemaWatcherBot.Domain.Entities;
+
+public class MovieSession
+{
+    public Guid Id { get; private set; }
+
+    public Guid MovieId { get; private set; }
+    public Movie Movie { get; private set; } = null!;
+
+    public DateOnly Date { get; private set; }
+    public TimeOnly Time { get; private set; }
+
+    public decimal Price { get; private set; }
+    public string Hall { get; private set; } = string.Empty;
+
+    private MovieSession() { }
+
+    public MovieSession(
+        Movie movie,
+        DateOnly date,
+        TimeOnly time,
+        decimal price,
+        string hall)
+    {
+        Id = Guid.NewGuid();
+        Movie = movie;
+        MovieId = movie.Id;
+        Date = date;
+        Time = time;
+        Price = price;
+        Hall = hall;
+    }
+}
